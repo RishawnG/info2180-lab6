c5eb6b0ee549ca6d448f30713a846e937061ebd7
Rishawn Gregory <rishawn.gregory@mymona.uwi.edu>
2017-11-02 03:15:47 +0000

    Exercise 4

--- request.php
+++ request.php
@@ -14,5 +14,44 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+
+    <definition name="definition" author="John">
+     A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <definition name="bar" author="Bob">
+        A place that sells alcholic beverages.
+    </definition>
+    
+    <definition name="ajax" author="Kim">
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition>
+    
+    <definition name="html" author="Alex">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    
+    <definition name="css" author="Mohammed">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    
+    <definition name="javascript" author="Bobby">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    
+    <definition name="php" author="Rian">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+    
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo  $xmlOutput->asXML();
+
+
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
